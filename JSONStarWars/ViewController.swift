07858be55a76a8ea32bd60105ca1d197a1c0a50f
Student <8Ball@macbook-pro-0459-rmiller.local>
2016-05-16 10:07:27 -0400

    First Commit
    Working on UI

---
+++ JSONStarWars/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  JSONStarWars
+//
+//  Created by Gorrila 8Ball Ribs on 2016-05-16.
+//  Copyright © 2016 Gorrila 8Ball Ribs. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
